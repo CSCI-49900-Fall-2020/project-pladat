4f133bc8e7cf5f9b10f027a86384161ef65fb4d2
William <williamd8323@afsenyc.org>
2020-09-25 18:58:38 -0400

    added more to schemas, and working on auth

---
+++ backend/configs/passport.js
@@ -0,0 +1,39 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
+module.exports = (passport) => {
+    passport.use(new LocalStrategy( {usernameField: 'email'}, (email, password, done) => {
+        User.findOne({email: email})
+        .then(user => {
+            if(!user) {
+                return done(null, false, {msg: 'Email is not registered'});
+            }
+            /* maybe check for verification here */
+
+            bcrypt.compare(password.trim(), user.password, (err, isMatch) => {
+                if(err) {
+                    throw err;
+                }
+                if(isMatch) {
+                    return done(null, user);
+                }
+                else {
+                    return done(null, false, { msg: 'Password is incorrect' });
+                }
+            });
+        })
+    } ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        })
+    })
+}
