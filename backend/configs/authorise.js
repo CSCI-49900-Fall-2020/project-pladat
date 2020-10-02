182d977a0628c9049b3be027dbadede9a3e71312
William <williamd8323@afsenyc.org>
2020-10-02 18:00:58 -0400

    .env now in git, authorisejs done, working on sendgrid email validation

---
+++ backend/configs/authorise.js
@@ -0,0 +1,20 @@
+module.exports = {
+    ensureAuthenticated: (req, res, next) => {
+        if(req.user) {
+            return next();
+        }
+        res.status(401).json({success: false, msg: 'Must be logged in to proceed'});
+    },
+    forwardAuthentication: (req, res, next) => {
+        if(!req.isAuthenticated()) {
+            return next();
+        }
+        res.status(200).json({success: false, msg: 'Successfully Logged In'}); 
+    },
+    ensureAuthorisation: (req, res, next) => {
+        if(req.user.typeOfUser === req.query.actionUserType) {
+            return next();
+        }
+        res.status(401).json({success: false, msg: 'This action is reserved for other users.'});
+    }
+}
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,10 +8,46 @@ const passport = require('passport');
 const User = require('../../models/User');
 const { forwardAuthentication, ensureAuthenticated, ensureAuthorisation } = require('../../configs/authorise');
 
+function verifyToken(req, res, next) {
+    req.token = req.query.token;
+    if(!req.token || typeof req.token === 'undefined' || typeof req.token === null) {
+        return res.status(403).json({success: false, msg: "Invalid email verification link, or your link has expired."});
+    }
+}
+
+function checkValidEmailFormat(email) {
+    const regex = /^([a-zA-Z0-9]+[a-zA-Z0-9.!#$%&'*+\-\/=?^_`{|}~]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/g;
+    let emailpass = regex.test(email);
+    return emailpass;
+}
 
 
-router.post('/register/:userType', (req, res) => {
-    res.status(200).json({success: true, msg: 'successful registeration'});
+router.post('/register', (req, res) => {
+    const { firstname, lastname, email, pass, passConf, userType } = req.body;
+    let isvalidemail = checkValidEmailFormat(email);
+
+    if(!firstname || !lastname || !email || !pass || !passConf || !userType) {
+        return res.status(422).json({success: false, msg: 'Please enter all fields.'});
+    }
+    else if(firstname.length < 1 || lastname.length < 1 || email.length < 1 || pass.length < 1 || passConf.length < 1|| userType.length < 1) {
+        return res.status(422).json({success: false, msg: 'Please enter all fields.'});
+    }
+    else if(!isvalidemail) {
+        return res.status(422).json({success: false, msg: 'Please enter a valid email address.'});
+    }
+    else if(pass.length < 8) {
+        return res.status(422).json({success: false, msg: 'Passwords should be at least 8 characters, and a mix of letters, numbers, and symbols'});
+    }
+    else if(pass !== passConf) {
+        return res.status(422).json({success: false, msg: 'Passwords must match.'});
+    }
+    else if(userType !== 'student' || userType !== 'recruiter' || userType !== 'employer') {
+        return res.status(422).json({success: false, msg: 'Invalid user type.'});
+    }
+    else {
+        // send verification email here
+    }
+
 });
 
 router.get('/register/verifyEmail', (req, res) => {
