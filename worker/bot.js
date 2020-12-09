e7ee0f97315b2ba7b12627a07071574f3e5fe4e9
William <williamd8323@afsenyc.org>
2020-12-09 04:36:08 -0500

    sdls

--- worker/bot.js
+++ worker/bot.js
@@ -42,7 +42,7 @@ mongoose
 // const workServer = workerApp.listen(PORT, () => {
 //     eventEmmiter.emit('startBot');
 //     console.log('worker running on port: ', PORT);
-// });
+// });//
 eventEmmiter.emit('startBot');
 
 
